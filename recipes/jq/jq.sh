fb605ce5ac47e490b5d234cd43069c52d1be9424
Dmytro Donukis <dmytro.donukis@cognite.com>
2023-11-14 18:20:13 +0100

    upd jq example

--- recipes/jq/jq.sh
+++ recipes/jq/jq.sh
@@ -2,6 +2,6 @@
 # 1. convert yaml to json
 # 2. create a list of key=value pairs
 yq -o=json data/functions/pycat/config.yaml \
-  | jq -c -r 'to_entries[] | (.key + "=", .value)' \
+  | jq -c -r 'to_entries | map(select(.value != null)) |.[] | (.key + "=", .value)'\
   | jq -R -s -r 'gsub("=\\n"; "=")'
 ```
